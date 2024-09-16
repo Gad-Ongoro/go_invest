778ead30af6c801ccf46b681c645c22b89740ce1
Gad-Ongoro <gadongoro1@gmail.com>
2024-09-16 05:01:25 +0300

    Added `User`, `InvestmentAccount`, `UserInvestmentAccount`, and `Transaction` models to the Django admin interface for easier management.

--- server/investments_api/admin.py
+++ server/investments_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, InvestmentAccount, UserInvestmentAccount, Transaction
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(InvestmentAccount)
+admin.site.register(UserInvestmentAccount)
+admin.site.register(Transaction)
