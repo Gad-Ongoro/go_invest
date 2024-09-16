22c742e4a0ca169727f0971d14b6b9bd26ca1c35
Gad-Ongoro <gadongoro1@gmail.com>
2024-09-16 07:42:02 +0300

    Implemented custom permissions for secure and role-based access control for transaction operations - Added `TransactionPermission` class to handle permission checks for transaction-related views. - Implemented logic to restrict access based on user roles and permissions associated with `InvestmentAccount`. - Added membership checks to ensure users are part of the relevant `InvestmentAccount` before allowing actions. - Defined permissions for different levels of access: view-only, full CRUD, and create-only. - Included error handling for cases where users are not part of the required investment account or lack sufficient permissions.

---
+++ server/investments_api/permissions.py
@@ -0,0 +1,56 @@
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
+from .models import InvestmentAccount, UserInvestmentAccount, User
+
+class TransactionPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        transaction_data = request.data
+        user_id = transaction_data.get('user')
+        user = user_id if user_id else request.user
+        account_id = view.kwargs.get('account_id')
+
+        if not account_id:
+            return False
+
+        # Membership restriction
+        try:
+            user_investment = UserInvestmentAccount.objects.get(user=user, investment_account=account_id)
+            account = user_investment.investment_account
+            user = User.objects.get(id=user)
+            user_groups = user.groups.all()
+            user_access_rights = [group.permissions.all()[0].codename for group in user.groups.all()]
+
+            if account.permission == InvestmentAccount.VIEW and 'can_only_read_transactions' in user_access_rights:
+                return request.method == 'GET' and user_groups.filter(name='view_group').exists()
+            elif account.permission == InvestmentAccount.FULL_CRUD and 'can_crud_transactions' in user_access_rights:
+                return user_groups.filter(name='crud_group').exists()
+            elif account.permission == InvestmentAccount.POST_ONLY and 'can_only_create_transactions' in user_access_rights:
+                return request.method == 'POST' and user_groups.filter(name='create_group').exists()
+            else:
+                raise PermissionDenied(detail='You do not have permission to perform this action.')
+
+        except UserInvestmentAccount.DoesNotExist:
+            raise PermissionDenied(detail='You are not a member of this investment account.')
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        account_id = obj.account.id
+
+        # Membership restriction
+        try:
+            user_investment = UserInvestmentAccount.objects.get(user=user, investment_account_id=account_id)
+            account = user_investment.investment_account
+
+            user_groups = user.groups.all()
+
+            if account.permission == InvestmentAccount.VIEW:
+                return request.method == 'GET' and user_groups.filter(name='view_group').exists()
+            elif account.permission == InvestmentAccount.FULL_CRUD:
+                return user_groups.filter(name='crud_group').exists()
+            elif account.permission == InvestmentAccount.POST_ONLY:
+                return request.method == 'POST' and user_groups.filter(name='create_group').exists()
+
+        except UserInvestmentAccount.DoesNotExist:
+            raise PermissionDenied(detail='You are not a member of this investment account.')
+
+        return False
